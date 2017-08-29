903adae831b9fc3964512e2e1ba3c814c3f3a28d
Rick Myers <slipdizzy@gmail.com>
2017-08-28 22:07:08 -0400

    Challenge Complete: Add a Previous Button

--- app/src/main/java/com/myerstyme/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/myerstyme/android/geoquiz/QuizActivity.java
@@ -12,6 +12,7 @@ public class QuizActivity extends AppCompatActivity {
     private Button mTrueButton;
     private Button mFalseButton;
     private Button mNextButton;
+    private Button mPrevButton;
     private TextView mQuestionTextView;
 
     private Question[] mQuestionBank = new Question[]{
@@ -50,6 +51,8 @@ public class QuizActivity extends AppCompatActivity {
         mTrueButton = (Button) findViewById(R.id.true_button);
         mFalseButton = (Button) findViewById(R.id.false_button);
         mNextButton = (Button) findViewById(R.id.next_button);
+        mPrevButton = (Button) findViewById(R.id.prev_button);
+
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
 
 
@@ -70,6 +73,21 @@ public class QuizActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                updateQuestion();
+            }
+        });
+
+        mPrevButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //decrement index
+                if (mCurrentIndex <= 0) {
+                    mCurrentIndex = mQuestionBank.length - 1;
+                }
+                else {
+                    --mCurrentIndex;
+                }
+                //update the question with the new index
                 updateQuestion();
             }
         });
